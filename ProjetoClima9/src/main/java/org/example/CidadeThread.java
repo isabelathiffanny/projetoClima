db5fcc7d70260594ebad9b8e50af651b677867d6
Isabela Thiffanny <118058379+isabelathiffanny@users.noreply.github.com>
2024-06-30 16:45:50 -0300

    adicionado o projeto com 9 threads

---
+++ ProjetoClima9/src/main/java/org/example/CidadeThread.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.example;
+
+/**
+ *
+ * @author Isabe
+ */
+
+public class CidadeThread implements Runnable{
+    private Cidade[] cidades;
+
+    public CidadeThread(Cidade[] cidades) {
+        this.cidades = cidades;
+    }
+
+    @Override
+    public void run() {
+        for (Cidade cidade : cidades) {
+
+            cidade.buscarPrevisao(); 
+
+            try{
+                
+                cidade.estatisticas.acquire();
+
+                cidade.mostrarEstatisticasDosDias(); 
+
+                cidade.estatisticas.release();
+            }catch(InterruptedException e){
+                System.out.println("Thread interrompida.");
+            }
+
+        }
+    }
+}
